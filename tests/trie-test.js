2300e0774f9afaab83b0e118a5f25d1718f980d7
Ophus Wong <ophus.wong@gmail.com>
2017-12-19 15:32:39 -0700

    Stopping point for suggest

--- tests/trie-test.js
+++ tests/trie-test.js
@@ -38,4 +38,27 @@ describe('Trie', () => {
       expect(trie.count).to.equal(2);
     });
   });
+
+  describe('SUGGEST', () => {
+    it('should return null if there are no suggestions', () => {
+      let suggestion = trie.suggest('dog');
+
+      expect(suggestion).to.eq(null);
+    });
+
+    it.only('should provide a suggestion in an array', () => {
+      trie.insert('amp');
+      let suggestion = trie.suggest('a');
+      expect(suggestion).to.deep.eq(['amp']);
+
+      // trie.insert('pizza');
+      // let suggestion = trie.suggest('piz');
+
+      // expect(suggestion).to.deep.eq(['pizza']);
+
+      // trie.insert('pizzeria');
+
+      // expect(suggestion).to.deep.eq(['pizza', 'pizzeria']);
+    });
+  });
 });

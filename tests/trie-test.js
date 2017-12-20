9bfd628b2933b5ef1b4375d9e60800f124d0b5c8
Ophus Wong <ophus.wong@gmail.com>
2017-12-19 19:46:06 -0700

    Add conditional to return null if no suggestions

--- tests/trie-test.js
+++ tests/trie-test.js
@@ -31,7 +31,7 @@ describe('Trie', () => {
       expect(trie.count).to.equal(1);
       trie.insert('thing');
       expect(trie.count).to.equal(2);
-    });
+    })
 
     it('should not add duplicate items', () => {
       trie.insert('pizza');
@@ -40,7 +40,7 @@ describe('Trie', () => {
       expect(trie.count).to.equal(2);
       trie.insert('pizza');
       expect(trie.count).to.equal(2);
-    });
+    })
   });
 
   describe('SUGGEST', () => {
@@ -48,7 +48,7 @@ describe('Trie', () => {
       let suggestion = trie.suggest('dog');
 
       expect(suggestion).to.eq(null);
-    });
+    })
 
     it('should provide a suggestion in an array', () => {
       trie.insert('pizza');
@@ -56,7 +56,7 @@ describe('Trie', () => {
       let suggestion = trie.suggest('piz');
 
       expect(suggestion).to.deep.eq(['pizza']);
-    });
+    })
 
     it('should return words with the same root', () => {
       trie.insert('pizza');
@@ -67,33 +67,33 @@ describe('Trie', () => {
       let suggestion = trie.suggest('piz');
 
       expect(suggestion).to.deep.eq(['pizza', 'pizzas', 'pizzaz', 'pizzeria']);
-    });
+    })
+  });
 
-    describe('DICTIONARY', () => {
-      it.skip('should have a populate method which can pull in an array of words', () => {
-        trie.populate(dictionary);
+  describe('DICTIONARY', () => {
+    it.skip('should have a populate method which can pull in an array of words', () => {
+      trie.populate(dictionary);
 
-        expect(trie.count).to.eq(235886);
+      expect(trie.count).to.eq(235886);
 
-        let suggestion = trie.suggest('piz');
-        expect(suggestion).to.deep.eq(['pize', 'pizza', 'pizzeria', 'pizzicato', 'pizzle'])
-      });
-    });
+      let suggestion = trie.suggest('piz');
+      expect(suggestion).to.deep.eq(['pize', 'pizza', 'pizzeria', 'pizzicato', 'pizzle'])
+    })
+  });
 
-    describe('SELECT', () => {
-      it('should move selected words to the front of suggested words', ()=> {
-        trie.populate(dictionary);
+  describe('SELECT', () => {
+    it('should move selected words to the front of suggested words', ()=> {
+      trie.populate(dictionary);
 
-        let suggestion1 = trie.suggest('piz');
-        //=> ["pize", "pizza", "pizzeria", "pizzicato", "pizzle", ...]
-        expect(suggestion1).to.deep.eq(['pize', 'pizza', 'pizzeria', 'pizzicato', 'pizzle']);
+      let suggestion1 = trie.suggest('piz');
+      //=> ["pize", "pizza", "pizzeria", "pizzicato", "pizzle", ...]
+      expect(suggestion1).to.deep.eq(['pize', 'pizza', 'pizzeria', 'pizzicato', 'pizzle']);
 
-        trie.select('pizzeria');
+      trie.select('pizzeria');
 
-        let suggestion2 = trie.suggest('piz');
-        // => ["pizzeria", "pize", "pizza", "pizzicato", "pizzle", ...]
-        expect(suggestion2).to.deep.eq([ 'pizzeria', 'pize', 'pizza', 'pizzicato', 'pizzle']);
-      });
+      let suggestion2 = trie.suggest('piz');
+      // => ["pizzeria", "pize", "pizza", "pizzicato", "pizzle", ...]
+      expect(suggestion2).to.deep.eq([ 'pizzeria', 'pize', 'pizza', 'pizzicato', 'pizzle']);
     })
   });
 });
